c4a6c8c8f2bd33027d77f244dbf1792bf2ba24d0
patkamon <patkamon.a@ku.th>
2023-03-16 21:09:59 +0700

    fix ? emoji

--- packages/client/src/Palette.tsx
+++ packages/client/src/Palette.tsx
@@ -9,7 +9,7 @@ export const Palette = ({ setcolor}: {setcolor : React.Dispatch<React.SetStateAc
     return (
       <div className="p-2 bg-white fixed left-0">
         {colors.map((_,index)=>{
-            return    <div key={index} className={`w-8 h-8 ${(colorTypes as any)[index].emoji} border border-black justify-center cursor-pointer hover:ring`}
+            return    <div key={index} className={`w-8 h-8 ${(colorTypes as any)[index]?.emoji} border border-black justify-center cursor-pointer hover:ring`}
             onClick={(e)=>{e.preventDefault
                 setcolor(index)
               }}
