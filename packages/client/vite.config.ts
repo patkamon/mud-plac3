e28d04219fce6c63827abb6332f46e5004bbd358
patkamon <patkamon.a@ku.th>
2023-03-22 22:33:19 +0700

    resolve rollup vite

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "graphql", replacement: "/graphql" }],
+ },
   server: {
     port: 3000,
     fs: {
